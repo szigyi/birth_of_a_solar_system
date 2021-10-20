95922f377bce3c1465001c773e3f68bf91ec2715
Sabi <szigyi.szogyi@gmail.com>
2021-10-20 21:39:43 +0100

    add proper gravity acting on bodies in space
    previous version used gravity as one simple force going downwards, but now the bodies act upon each other, kidna

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-const Mover = (locationParam, velocityParam, accelerationParam, massParam) => {
-  let location = locationParam
-  let velocity = velocityParam
-  let acceleration = accelerationParam
-  let mass = massParam
+const Mover = (name, mass, x, y) => {
+  let location = createVector(x, y)
+  let velocity = createVector(0, 0)
+  let acceleration = createVector(0, 0)
 
   return ({
+    name: () => name,
     location: () => location,
+    mass: () => mass,
 
     applyForce: (force) => {
       force.div(mass)
@@ -21,7 +22,7 @@ const Mover = (locationParam, velocityParam, accelerationParam, massParam) => {
     display: () => {
       stroke(0)
       fill(175)
-      ellipse(location.x, location.y, mass * 16, mass * 16)
+      ellipse(location.x, location.y, mass * 1000, mass * 1000)
     },
 
     checkEdges: () => {
@@ -36,29 +37,61 @@ const Mover = (locationParam, velocityParam, accelerationParam, massParam) => {
       if (location.y > height) {
         velocity.y *= -1;
         location.y = height;
+      } else if (location.y < 0) {
+        velocity.y *= -1
+        location.y = 0
       }
     }
   })
 };
 
+function applyGravity(gravity, bodies) {
+  bodies.forEach(body => {
+    bodies.forEach(effectedBody => {
+      if (body.name !== effectedBody.name) {
+        const m1 = body.mass()
+        const m2 = effectedBody.mass()
+        // console.log('m1:', m1)
+        // console.log('m2:', m2)
+        const r = body.location().dist(effectedBody.location())
+        // console.log('r:', r)
+        const force = gravity * ((m1 * m2) / (r * r))
+        // console.log('force:', force)
+        const vectorForce = p5.Vector.sub(body.location(), effectedBody.location()).mult(force)
+        // console.log('vectorForce:', vectorForce)
+        effectedBody.applyForce(vectorForce)
+      }
+    })
+  })
+}
 
-let v1;
-let wind;
+function update(bodies) {
+  bodies.forEach(body => body.update())
+}
+
+function display(bodies) {
+  bodies.forEach(body => body.display())
+}
+
+function checkEdges(bodies) {
+  bodies.forEach(body => body.checkEdges())
+}
+
+
+let bodies;
 let gravity;
 
 function setup() {
-  createCanvas(400, 400);
-  v1 = Mover(createVector(0, 10), createVector(0, 0), createVector(0, 0), 1)
-  wind = createVector(0.01, 0)
-  gravity = createVector(0, 0.1)
+  createCanvas(800, 800);
+  gravity = 9.8
+  bodies = [Mover('Sun', 0.01, 50, 20), Mover('Earth', 0.05, 260, 260)]
 }
 
 function draw() {
   background(220);
 
-  v1.applyForce(wind)
-  v1.applyForce(gravity)
-  v1.update()
-  v1.display()
-  v1.checkEdges()
+  applyGravity(gravity, bodies)
+  update(bodies)
+  display(bodies)
+  checkEdges(bodies)
 }
